de81d5a1ae4737561dcae979fedd9476c0706cf4
Balaji Sivakumar <balajisivakumar@Balajis-Macbook.ht.home>
2024-05-19 21:54:16 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myString = "Hello";
+Console.WriteLine(myString[1]);  // Outputs "e"
+
